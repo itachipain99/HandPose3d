d98f31363b32a7b281ca5bf1c9d3d7682a498752
itachipain99 <itachipain11@gmail.com>
2020-11-11 15:56:05 +0700

    Initial Commit

---
+++ TestVNHandPose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestVNHandPose
+//
+//  Created by Nguyễn Minh Hiếu on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
